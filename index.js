16b1af9b7b2d8c752d47a3aecebe2a44be4d3654
heidiwu3388 <heidiwu3388@yahoo.com>
2022-12-03 21:24:46 -0800

    add comments to the code

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// bring in all the libraries
 const inquirer = require("inquirer");
 const chalk = require("chalk");
 const fs = require("fs");
@@ -21,16 +22,20 @@ function buildTeam() {
     console.log(chalk.blue("* Welcome to the Team Profile Generator! *"));
     console.log(chalk.blue("******************************************"));
     console.log(chalk.blue("Let's build your team now:"));
-    
+    // start with a set of questions for the manager
     inquirer.prompt(questionsForManager)
     .then ((answers) => {
+        // create a new Manager by input data
         const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
+        // add the newly created manager to the team
         team.addMember(manager);
+        // ask if done or want to add some more team members
         askToContinue();        
     })
 }
 
 function askToContinue() {
+    // ask user to choose from a list [Engineer, Intern, quit]
     inquirer.prompt(questionForContinue)
     .then((answers) => {
         if (answers.choice === "Engineer") {
@@ -49,6 +54,7 @@ function askToContinue() {
                 } else {
                     console.log(chalk.green("HTML and CSS are generated successfully in the ./dist/ folder!!!!"));
                 }
+                // exit the application
                 process.exit(0);
             });
         }
@@ -59,10 +65,14 @@ function askEngineerInfo() {
     console.log(chalk.green("============="));
     console.log(chalk.green("   Engineer  "));
     console.log(chalk.green("============="));
+    // ask user to input the engineer's information
     inquirer.prompt(questionsForEngineer)
     .then ((answers) => {
+        // create a new Engineer by user input
         const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github)
+        // add the newly created Engineer to the team
         team.addMember(engineer);
+        // ask if done or want to add some more team members
         askToContinue();
     })
 }
@@ -71,16 +81,21 @@ function askInternInfo() {
     console.log(chalk.magenta("==========="));
     console.log(chalk.magenta("   Intern  "));
     console.log(chalk.magenta("==========="));
+    // ask user to input the intern's information
     inquirer.prompt(questionsForIntern)
     .then ((answers) => {
+        // create a new Intern by user input
         const intern = new Intern (answers.name, answers.id, answers.email, answers.school)
+        // add the newly created Intern to the team
         team.addMember(intern);
+        // ask if done or want to add some more team members
         askToContinue();        
     })
 }
 
 
 function addCardsToPage(cards) {
+    // return a HTML page with dynamically generated cards passing as a parameter
     return `
     <!DOCTYPE html>
     <html lang="en">
